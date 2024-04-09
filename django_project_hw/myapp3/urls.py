9a2b3d400187916474bed8854b3e902490ed65be
Ilya Plutov <ilya.plutov@gmail.com>
2024-04-09 21:31:42 +0300

    added details on hw3

--- django_project_hw/myapp3/urls.py
+++ django_project_hw/myapp3/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<int:client_id>/last_week', views.last_order_products, {'argument': 'week'}, name='last_week'),
+    path('<int:client_id>/last_month', views.last_order_products, {'argument': 'month'}, name='last_week'),
+    path('<int:client_id>/last_year', views.last_order_products, {'argument': 'year'}, name='last_week'),
 ]
